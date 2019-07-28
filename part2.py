00b8d8b1ce4937ffff64715f6934118e21a09c3a
myname21-meet <myname21@meet.mit.edu>
2019-07-28 11:18:55 -0400

    python

--- part2.py
+++ part2.py
@@ -1,7 +1,13 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	if x not in range(500,1000) and y not in range(1,250):
+			print("Invalid input: outside range")
+	else: 
+		return(x * y)
+
+print(encode(300, 300))
+
 
 def decode(coded_message):
 	pass
